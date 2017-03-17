628d261ccd8bed6cb496373487aabb7f89ee7039
Paul Haddad <paulh16@gmail.com>
2017-03-17 06:30:40 -0500

    Video 18: Persisting Our State With Firebase * Connect the fish state to Firebase * Hook into two of React's lifecycle hooks

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBnevqru0Dtbl3we6s54V7VZL87onAuG9o",
+  authDomain: "catch-of-the-day-paul-haddad.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-paul-haddad.firebaseio.com",
+});
+
+export default base;
